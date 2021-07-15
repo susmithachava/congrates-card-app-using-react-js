4d6b3f65d57bdd6d88d25a45da8105575a249866
root <susmi.chava@gmail.com>
2021-07-15 13:39:52 +0000

    congrates card app using react js

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  // Write your code here.
+  <div className="background">
+    <h1>Congratulations</h1>
+    <div className="inner-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 className="mini-heading">Kiran V</h1>
+      <p className="mini-para">
+        Vishnu Institute of Computer Education and technology, Bhimavaram
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
